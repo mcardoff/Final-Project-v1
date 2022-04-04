f3b414aefe3a27b0a023c64c336bae8d7b894833
Michael Cardiff <mcardiff@hawk.iit.edu>
2022-04-04 17:52:32 -0500

    Initial Commit

---
+++ Shared/Final_Project_V1App.swift
@@ -0,0 +1,17 @@
+//
+//  Final_Project_V1App.swift
+//  Shared
+//
+//  Created by Michael Cardiff on 4/4/22.
+//
+
+import SwiftUI
+
+@main
+struct Final_Project_V1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
