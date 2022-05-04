a1843a36c2f3fa31df2c25c16cd56d3fae4b11ef
Michael Cardiff <mcardiff@hawk.iit.edu>
2022-05-04 17:08:19 -0500

    Start Gradient Descent

--- Shared/GradientDescent.swift
+++ Shared/GradientDescent.swift
@@ -8,5 +8,14 @@
 import Foundation
 
 class GradientDescent: OptimizationMethod {
+    var learningRate : Double
     
+    init(_ rate: Double) {
+        learningRate = rate // determines by how much you should descend
+    }
+    func minimize(problem: inout Problem, endCriteria: EndCriteria) -> EndCriteriaType {
+        // get current problem
+        var ecType = EndCriteriaType.None
+        return ecType
+    }
 }
